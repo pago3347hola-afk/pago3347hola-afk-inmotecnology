713b41b6dd7c780458336f643ecda1df9458df5e
Jose Lopez <pago3347hola@gmail.com>
2025-06-27 08:30:53 +0000

    Necesito que pongas la siguiente imagen pero antes necesito que elimines

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -14,7 +14,7 @@ export default function PropertiesPage() {
         <Card className="overflow-hidden shadow-lg rounded-xl">
           <CardHeader className="p-0">
              <Image
-              src="https://placehold.co/800x500.png"
+              src="__GCP_BUCKET_URL__/casa.png"
               data-ai-hint="house pool"
               alt="Casa Bosquesinos"
               width={800}
