15220cbcf845a5d6564da62a4f2e9577e714a790
Jose Lopez <pago3347hola@gmail.com>
2025-06-27 11:25:45 +0000

    el problema se origina cuando le pico en mi pagina web en iniciar sesion

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,11 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// IMPORTANT: Replace the following with your web app's Firebase configuration
-// which you can find in your project's settings on the Firebase console.
 const firebaseConfig = {
   apiKey: "AIzaSyCVfF4LlvpIsap-vfuplbhBMBXvz_dT47g",
   authDomain: "inmotecnologa-hub.firebaseapp.com",
   projectId: "inmotecnologa-hub",
-  storageBucket: "inmotecnologa-hub.appspot.com",
+  storageBucket: "inmotecnologa-hub.firebasestorage.app",
   messagingSenderId: "755441536028",
   appId: "1:755441536028:web:1c336f159254098e5be3d9"
 };
