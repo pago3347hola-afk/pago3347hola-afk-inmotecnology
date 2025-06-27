af75d73d51ef005f1478014f41b565bf4f060b97
Jose Lopez <pago3347hola@gmail.com>
2025-06-27 09:03:16 +0000

    Cambia Casa de Campo con Alberca a casa con alberca

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -16,13 +16,13 @@ export default function PropertiesPage() {
              <Image
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu1o0NyUqFdZF6kxB1JvRge3lrCkSlHlRwMvAGPQeZ5WLiCKazKrGJiKAF&s=10"
               data-ai-hint="modern house pool"
-              alt="Casa moderna con alberca"
+              alt="casa con alberca"
               width={800}
               height={500}
               className="w-full h-auto object-cover"
             />
             <div className="p-6">
-                <CardTitle className="font-headline text-3xl">Casa de Campo con Alberca</CardTitle>
+                <CardTitle className="font-headline text-3xl">casa con alberca</CardTitle>
             </div>
           </CardHeader>
           <CardContent className="p-6 space-y-6">
